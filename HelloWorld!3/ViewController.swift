7234bfbec099e0ccaa8086ee7007888f6d2eb2f9
Ilona Shcherbakova <shcherbakova0911@mail.ru>
2019-09-22 11:51:09 +0300

    Initial Commit

---
+++ HelloWorld!3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld!3
+//
+//  Created by Ilona Shcherbakova on 22/09/2019.
+//  Copyright © 2019 Ilona Shcherbakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
